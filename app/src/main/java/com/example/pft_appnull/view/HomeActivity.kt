1a331e293ece5d8cad494d2b99760323c98d458d
francoscaronefgsm <franco.scarone@estudiantes.utec.edu.uy>
2024-07-10 20:31:48 -0300

    app andando

--- app/src/main/java/com/example/pft_appnull/view/HomeActivity.kt
+++ app/src/main/java/com/example/pft_appnull/view/HomeActivity.kt
@@ -2,39 +2,18 @@ package com.example.pft_appnull.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.example.pft_appnull.R
-import com.example.pft_appnull.utils.PreferenceHelper
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.gson.Gson
 
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var nav : BottomNavigationView
 
-    private val preferences by lazy {
-        PreferenceHelper.defaultPrefs(this)
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if(isAnalyst()) {
-            setContentView(R.layout.activity_home_analista)
-            Log.e("entra al if", "ES ANALISTA")
-            nav = findViewById(R.id.nav)
-            nav.setOnNavigationItemSelectedListener { item ->
-                when(item.itemId){
-                    R.id.home -> { supportFragmentManager.beginTransaction().replace(R.id.container,
-                        HomeFragment()
-                    ).commit()}
-                    R.id.lista_reclamos -> { supportFragmentManager.beginTransaction().replace(R.id.container,
-                        TodosReclamosAnalistaFragment()
-                    ).commit()}
-                }
-                true
-            }
-        }
-        else{
+
             setContentView(R.layout.activity_home_estudiante)
 
             nav = findViewById(R.id.nav)
@@ -52,23 +31,12 @@ class HomeActivity : AppCompatActivity() {
                 }
                 true
             }
-        }
+
         if (savedInstanceState == null){
             supportFragmentManager.beginTransaction().replace(R.id.container,
                 HomeFragment()
             ).commit()}
         }
 
-
-    private fun isAnalyst(): Boolean {
-        val usuarioJson = preferences.getString("usuarioDTO", "")
-        if (usuarioJson != null) {
-            if (usuarioJson.isNotEmpty()) {
-                val usuarioDTO = Gson().fromJson(usuarioJson, UsuarioDTORest::class.java)
-                return usuarioDTO.tipo == "Analista"
-            }
-        }
-        return false
-    }
 
 }
